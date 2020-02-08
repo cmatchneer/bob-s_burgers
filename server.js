732c11283ecf5b121a2be11d28e6738597e615da
cmatchneer <cmatchneer@yahoo.com>
2020-02-08 14:10:27 -0500

    more fixes

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-var routes = require("/controllers/burger_controller");
+var routes = require("./controllers/burger_controller");
 app.use(express.static(__dirname + "/public/css"));
 app.use(express.static(__dirname + "/public/js"));
-app.use(express.static("public"));
+
 console.log(__dirname + "/app/public/css");
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
